069531640680765dd4278e1c18f071d46fd0e7df
d.miroshnichenko <dmamiruaa@gmail.com>
2024-03-22 07:34:12 +0200

    first commit

---
+++ src/main/java/HelloWorldApp.java
@@ -0,0 +1,28 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+
+public class HelloWorldApp extends AbstractHandler {
+    @Override
+    public void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
+        httpServletResponse.setContentType("text/html");
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        httpServletResponse.getWriter().println("<h1>Hello, World!</h1>");
+        httpServletResponse.getWriter().flush();
+        httpServletResponse.getWriter().close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8081);
+        server.setHandler(new HelloWorldApp());
+        server.start();
+        server.join();
+    }
+
+}
